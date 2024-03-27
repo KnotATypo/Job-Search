ad76b4de54c1387c01e17a6b9ed72989597deb0c
Josh <joshua.mackay17@gmail.com>
2024-03-27 11:01:13 +1000

    Improving time estimates

--- applying.py
+++ applying.py
@@ -1,23 +1,28 @@
 import os
 import sqlite3
 
-from sites import Seek
+from sites import Seek, Jora
 
 
 def main():
     seek = Seek(None)
+    jora = Jora(None)
     connection = sqlite3.connect('jobs.db')
     cursor = connection.cursor()
     jobs = cursor.execute("SELECT * FROM jobs WHERE status='interested_read'").fetchall()
     for job in jobs:
         choice = 0
-        print(f'{job[1]} - {job[2]}\n{seek.build_job_link(job[0])}')
+        if job[6] == 'seek':
+            link = seek.build_job_link(job[0])
+        elif job[6] == 'jora':
+            link = jora.build_job_link(job[0])
+        print(f'{job[1]} - {job[2]}\n{link}')
         while choice not in {'y', 'n', 'not available'}:
             choice = input('Have you applied? [y/n/not available]: ')
         if choice == 'y':
-            cursor.execute(f"UPDATE jobs SET status='applied' WHERE id={job[0]}")
+            cursor.execute(f"UPDATE jobs SET status='applied' WHERE id='{str(job[0])}'")
         if choice in {'not available'}:
-            cursor.execute(f"UPDATE jobs SET status='not_interested' WHERE id={job[0]}")
+            cursor.execute(f"UPDATE jobs SET status='not_interested' WHERE id='{str(job[0])}'")
             try:
                 os.remove(f'job_descriptions/{job[3]}')
             except FileNotFoundError:
--- sites.py
+++ sites.py
@@ -1,7 +1,8 @@
 import asyncio
+import re
 import sqlite3
 from collections import namedtuple
-from typing import List, Any
+from typing import List
 
 import requests
 from bs4 import BeautifulSoup
@@ -15,26 +16,58 @@ JobDetails = namedtuple('JobDetails', ['id', 'title', 'company'])
 class Site:
     BASE_URL: str
     JOB_URL: str
+    site_string: str
     cursor: sqlite3.Cursor
     connection: sqlite3.Connection
 
     def download_new_jobs(self, query) -> None:
-        raise NotImplemented
+        print(f'Searching on {self.site_string} for "{query}"')
+        jobs = self.list_all_jobs(query)
+        jobs = self.remove_duplicates(jobs)
+
+        if len(jobs) == 0:
+            return
+        for i in tqdm(jobs, desc='Getting jobs', unit='job'):
+            # Removing the ' because it screws with db stuff
+            i = JobDetails(i.id, i.title.replace("'", ""), i.company.replace("'", ""))
+            file_name = f'{i[1]}-{i[2]}-{i[0]}.html'.replace('/', '_')
+            with open('job_descriptions/' + file_name, 'w+') as f:
+                try:
+                    f.write(self.get_job_description(i[0]))
+                except UnicodeEncodeError:
+                    continue
+            self.cursor.execute(
+                f"INSERT INTO jobs VALUES('{i.id}', '{i.title}', '{i.company}', '{file_name}', null, 'new', '{self.site_string.lower()}')")
+            self.connection.commit()
 
     def list_all_jobs(self, query) -> List[JobDetails]:
         page = 0
         jobs = []
         new_results = True
-        with tqdm(total=1, desc='Search pages', unit='page') as pbar:
+
+        fetchone = self.cursor.execute(f"SELECT pages FROM page_size WHERE site = '{self.site_string.lower()}' and search = '{query}'").fetchone()
+        if fetchone is None:
+            expected_pages = 1
+        else:
+            expected_pages = fetchone[0]
+
+        with tqdm(total=expected_pages, desc='Search pages', unit='page') as pbar:
             while new_results:
                 page += 1
                 this_page = self.get_jobs_from_page(page, query)
                 jobs.extend(this_page)
                 if len(this_page) == 0:
                     new_results = False
-                else:
-                    pbar.total = page + 1
+                if page > expected_pages:
+                    pbar.total += 1
                 pbar.update()
+
+        if expected_pages > 1:
+            self.cursor.execute(f"UPDATE page_size SET pages = '{page}' WHERE site = '{self.site_string.lower()}' and search = '{query}'")
+        else:
+            self.cursor.execute(f"INSERT INTO page_size VALUES ('{self.site_string.lower()}', '{query}', '{page}')")
+        self.connection.commit()
+
         return jobs
 
     def get_jobs_from_page(self, page_number, query) -> List[JobDetails]:
@@ -68,28 +101,11 @@ class Seek(Site):
     def __init__(self, db_connection):
         self.BASE_URL = 'https://www.seek.com.au/'
         self.JOB_URL = 'https://www.seek.com.au/job/%%ID%%'
+        self.site_string = 'Seek'
 
         if db_connection is not None:
             self.connection = db_connection
             self.cursor = db_connection.cursor()
-
-    def download_new_jobs(self, query) -> None:
-        print(f'Searching on Seek for "{query}"')
-        jobs = self.list_all_jobs(query)
-        jobs = self.remove_duplicates(jobs)
-
-        for i in tqdm(jobs, desc='Getting jobs', unit='job'):
-            # Removing the ' because it screws with db stuff
-            i = JobDetails(i.id, i.title.replace("'", ""), i.company.replace("'", ""))
-            file_name = f'{i[1]}-{i[2]}-{i[0]}.html'.replace('/', '_')
-            with open('job_descriptions/' + file_name, 'w+') as f:
-                try:
-                    f.write(self.get_job_description(i[0]))
-                except UnicodeEncodeError:
-                    continue
-            self.cursor.execute(
-                f"INSERT INTO jobs VALUES('{i.id}', '{i.title}', '{i.company}', '{file_name}', false, 'new', 'seek')")
-            self.connection.commit()
 
     def get_job_description(self, job_id) -> str | None:
         response = requests.get(self.build_job_link(job_id))
@@ -101,7 +117,8 @@ class Seek(Site):
             return None
 
     def get_jobs_from_page(self, page_number, query) -> List[JobDetails]:
-        response = requests.get(self.BASE_URL + f'{query}-jobs/in-Brisbane-CBD-&-Inner-Suburbs-Brisbane-QLD?page={page_number}')
+        response = requests.get(
+            self.BASE_URL + f'{query}-jobs/in-Brisbane-CBD-&-Inner-Suburbs-Brisbane-QLD?page={page_number}')
         if response.status_code != 200:
             print('The response returned a non-200 status.')
 
@@ -125,28 +142,11 @@ class Jora(Site):
     def __init__(self, db_connection):
         self.BASE_URL = 'https://au.jora.com/j?l=Brisbane+QLD&q='
         self.JOB_URL = 'https://au.jora.com/job/%%ID%%'
+        self.site_string = 'Jora'
 
         if db_connection is not None:
             self.connection = db_connection
             self.cursor = db_connection.cursor()
-
-    def download_new_jobs(self, query) -> None:
-        print(f'Searching on Jora for "{query}"')
-        jobs = self.list_all_jobs(query)
-        jobs = self.remove_duplicates(jobs)
-
-        for i in tqdm(jobs, desc='Getting jobs', unit='job'):
-            # Removing the ' because it screws with db stuff
-            i = JobDetails(i.id, i.title.replace("'", ""), i.company.replace("'", ""))
-            file_name = f'{i[1]}-{i[2]}-{i[0]}.html'.replace('/', '_')
-            with open('job_descriptions/' + file_name, 'w+') as f:
-                try:
-                    f.write(self.get_job_description(i[0]))
-                except UnicodeEncodeError:
-                    continue
-            self.cursor.execute(
-                f"INSERT INTO jobs VALUES('{i.id}', '{i.title}', '{i.company}', '{file_name}', false, 'new', 'jora')")
-            self.connection.commit()
 
     def get_job_description(self, job_id) -> str | None:
         loop = asyncio.get_event_loop()
@@ -165,10 +165,11 @@ class Jora(Site):
         soup = BeautifulSoup(content, features="html.parser")
         if soup.text.find('We have looked through all the results for you') != -1:
             return []
+        last_page = int(re.findall(r'\d+', soup.find_all('div', 'search-results-count')[0].text)[-1])
+        if page_number > last_page:
+            return []
         matches = soup.find_all('a', attrs={'class': 'job-link -no-underline -desktop-only show-job-description'})
         matches = [self.extract_job_info(x) for x in matches]
-        if len(matches) < 10:
-            return []
         return matches
 
     def extract_job_info(self, job) -> JobDetails:
