9bf487a3100f2338358124bfbe46d693fc004c5e
Josh <joshua.mackay17@gmail.com>
2024-05-02 08:19:11 +1000

    files

--- src/interest_filter.py
+++ src/interest_filter.py
@@ -8,18 +8,21 @@ def main():
     connection.autocommit = True
     cursor = connection.cursor()
 
-    cursor.execute("SELECT title, file, id, duplicate FROM job_search WHERE status='new'")
+    cursor.execute("SELECT title, file, id, duplicate, company FROM job_search WHERE status='new'")
     jobs = cursor.fetchall()
 
     jobs.sort(key=lambda x: x[1])
-    for i, (name, file, id, duplicate) in enumerate(jobs):
+    for i, (name, file, id, duplicate, company) in enumerate(jobs):
         choice = 0
         print(f'{i}/{len(jobs)}')
         if duplicate is not None:
             dup_status = util.get_duplicate_status(id, cursor)
+            if dup_status in ('applied', 'interested_read', 'interested') and company != 'Private Advertiser':
+                print(f'{name} - {company}')
+                choice = 'n'
             print('Duplicate status:', dup_status)
         while choice not in {'n', 'y'}:
-            choice = input(f'{name}\nAre you interested? [y/n/undo]: ')
+            choice = input(f'{name}\n{company}\nAre you interested? [y/n/undo]: ')
             choice = choice.lower()
             if choice == 'undo':
                 cursor.execute(f"UPDATE job_search SET status='new' WHERE file='{jobs[i - 1][1]}'")
