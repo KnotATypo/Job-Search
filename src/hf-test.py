d7ea9bcaa1e23d6088980a1f20d762c08ed5d4de
Josh <joshua.mackay17@gmail.com>
2024-12-06 11:38:36 +1000

    Generating summaries

--- src/hf-test.py
+++ src/hf-test.py
@@ -1,34 +1,57 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausalLM
+import socket
+
+from fabric import Connection
+from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from model import Listing
+
+model_name = "Qwen/Qwen2.5-1.5B-Instruct"
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 
 def main():
-    # pipe = pipeline("translation", model="google-t5/t5-small")
-    # tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
-    # model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+    hostname = socket.getfqdn()
+    ip = socket.gethostbyname_ex(hostname)[2][1]
 
-    with open("../job_descriptions/Full Stack Developer-Clicks IT Recruitment (QLD)-74177500.html") as f:
-        desc = f.read()
+    listings = Listing.select().where(Listing.summary == "").execute()
+    if ip == "192.168.4.109":
+        for listing in tqdm(listings):
+            with open(f"/home/josh/Job-Search/descriptions/{listing.id}.txt") as f:
+                description = f.read()
+                if description == "":
+                    continue
+                response = summary(str(description))
+    else:
+        with Connection("jobs.lan", "josh") as c, c.sftp() as sftp:
+            for listing in tqdm(listings):
+                with sftp.open(f"Job-Search/descriptions/{listing.id}.txt") as f:
+                    description = f.read()
+                    if description == "":
+                        continue
+                    response = summary(str(description))
 
-    model_name = "Qwen/Qwen2.5-1.5B-Instruct"
 
-    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+# Please create a single sentence summary of this job description.
 
-    # prompt = "Give me a short introduction to large language model."
+
+def summary(description):
     messages = [
-        {"role": "system", "content": "Please create a single sentence summary of this job description."},
-        {"role": "user", "content": desc},
+        {
+            "role": "system",
+            "content": "Please create a single sentence summary of this job description without any corporate fluff",
+        },
+        {"role": "user", "content": description},
     ]
     text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
     model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-
     generated_ids = model.generate(**model_inputs, max_new_tokens=512)
     generated_ids = [
         output_ids[len(input_ids) :] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
     ]
-
     response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    print(response)
+    return response
 
 
 if __name__ == "__main__":
