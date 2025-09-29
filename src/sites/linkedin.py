0d5ae9bb7861296caf3362360e0c3d24c1627faa
Josh <joshua.mackay17@gmail.com>
2025-09-30 00:16:19 +1000

    Linkedin keeps giving american results, I think it's fixed now

--- src/sites/linkedin.py
+++ src/sites/linkedin.py
@@ -9,7 +9,7 @@ class LinkedIn(Site):
     def __init__(self):
         super().__init__(
             "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=%%QUERY%%&start=%%PAGE%%&geoId=101452733",
-            "https://www.linkedin.com/jobs/view/%%ID%%/",
+            "https://au.linkedin.com/jobs/view/%%ID%%/",
             "LinkedIn",
         )
 
@@ -25,13 +25,16 @@ class LinkedIn(Site):
             return []
 
         jobs = [self.extract_info(card) for card in cards]
+        jobs = [job for job in jobs if job is not None]
 
         return jobs
 
-    def extract_info(self, job) -> Tuple[Listing, Job]:
+    def extract_info(self, job) -> Tuple[Listing, Job] | None:
         links = job.find_all("a")
         link = links[0]["href"]
-        listing_id = link[35 : link[35:].index("?") + 35]
+        if "https://au" not in link:
+            return None
+        listing_id = link[link[: link.index("?")].rindex("/") + 1 : link.index("?")]
         title = links[0].text.strip()
         if len(links) == 1:
             company = "None"
