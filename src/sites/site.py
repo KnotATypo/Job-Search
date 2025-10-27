cb0c246db6a77a7d104c5b8b975c2b22ce1995f9
Josh <joshua.mackay17@gmail.com>
2025-10-28 00:08:26 +1000

    Cleaned up site class

--- src/sites/site.py
+++ src/sites/site.py
@@ -11,16 +11,33 @@ HTML_PARSER = "html.parser"
 
 
 class Site:
-    PAGE_URL: str
+    """
+    Superclass for all sites.
+    """
+
+    QUERY_URL: str
     LISTING_URL: str
     SITE_STRING: str
 
-    def __init__(self, page_url: str, listing_url: str, site_string: str) -> None:
-        self.PAGE_URL = page_url
+    def __init__(self, query_url: str, listing_url: str, site_string: str) -> None:
+        """
+        Constructor.
+
+        query_url -- The URL template of search page to retrieve job listings from. The expected template parameters are %%QUERY%% where the query string goes, and %%PAGE%% where the page number goes.
+        listing_url -- The URL template of individual listing to retrieve. The expect template parameter is %%ID%% where the id of the job listing goes.
+        site_string -- The plain text name of the site.
+        """
+        self.QUERY_URL = query_url
         self.LISTING_URL = listing_url
         self.SITE_STRING = site_string.lower()
 
     def download_new_listings(self, query: str, username: str) -> None:
+        """
+        Download new listings by iterating through pages using the QUERY_URL, incrementing the page.
+
+        query -- The search query.
+        username -- The username of the user to retrieve listings for.
+        """
         page_count: PageCount = PageCount.get_or_create(site=self.SITE_STRING, query=query)[0]
         expected_pages = page_count.pages
 
@@ -45,6 +62,13 @@ class Site:
         page_count.save()
 
     def save_listings(self, listings: List[Tuple[Listing, Job]], username):
+        """
+        Saves the provided listings and jobs into the database and writes the body of the listing to the filesystem.
+
+        listings -- List of tuples pairing Listings to their Job.
+        username -- The username of the user to save the jobs for.
+        """
+
         def strip_string(s: str) -> str:
             return re.sub(r"\W", "", s.lower())
 
@@ -68,18 +92,37 @@ class Site:
                 JobToListing.create(job_id=job.id, listing_id=listing.id)
 
     def build_page_link(self, page_number: int, query: str):
-        return self.PAGE_URL.replace("%%QUERY%%", query).replace("%%PAGE%%", str(page_number))
+        return self.QUERY_URL.replace("%%QUERY%%", query).replace("%%PAGE%%", str(page_number))
 
     def get_listings_from_page(self, page_number, query: str) -> List[Tuple[Listing, Job]]:
+        """
+        Retrieves (Listing, Job) tuples from a given page number.
+
+        page_number -- The page number of the search. For sites which require increments larger and +1 are handled inside this function.
+        query -- The search query.
+        """
         raise NotImplementedError
 
     def extract_info(self, job) -> Tuple[Listing, Job]:
+        """
+        Extracts information from BeautifulSoup page element into a (Listing, Job) tuple.
+
+        job -- Element to extract information from.
+        """
         raise NotImplementedError
 
     def build_job_link(self, job_id) -> str:
+        """
+        Builds a link to an individual job page using templated LISTING_URL.
+
+        job_id -- The job id of the individual job page.
+        """
         return self.LISTING_URL.replace("%%ID%%", str(job_id))
 
     def get_listing_description(self, listing_id) -> str:
+        """
+        Retrieves the description of the listing. Returns plain text.
+        """
         raise NotImplementedError
 
     @classmethod
