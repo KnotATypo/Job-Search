a31c3ab30c2c3407c7c63f7f25ec7cd8fe1bcc78
Josh <joshua.mackay17@gmail.com>
2025-10-27 23:30:54 +1000

    Writing placeholder summary to filtered jobs for easier distinction

--- src/create_summary.py
+++ src/create_summary.py
@@ -10,6 +10,9 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 def main():
     listings = Listing.select(Listing, JobToListing).join(JobToListing).execute()
+    for l in [l for l in listings if (l.jobtolisting.job_id.status == "easy_filter") and l.summary == ""]:
+        l.summary = "blacklist"
+        l.save()
     listings = [l for l in listings if (l.jobtolisting.job_id.status in ["new", "interested"]) and l.summary == ""]
     for listing in tqdm(listings):
         try:
