9e8309f45ca5c3054a060fb2568221be8c926d32
Josh <joshua.mackay17@gmail.com>
2026-01-13 11:06:34 +1000

    Overhauled the logic for deciding what needs to be summarised to better handle multiple users with the same/similar searches

--- src/job_search/create_summary.py
+++ src/job_search/create_summary.py
@@ -1,11 +1,12 @@
 import os
 
+import psycopg2
 from dotenv import load_dotenv
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 from job_search import util
-from job_search.model import Listing, JobToListing, Job
+from job_search.model import Listing
 
 load_dotenv()
 
@@ -15,24 +16,51 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 
 def main():
-    blacklist_listings = (
-        Listing.select().join(JobToListing).join(Job).where(Job.status == "blacklist", Listing.summary == "").execute()
+    connection = psycopg2.connect(
+        host=os.getenv("DATABASE_HOST"),
+        database=os.getenv("DATABASE_NAME"),
+        user=os.getenv("DATABASE_USER"),
+        password=os.getenv("DATABASE_PASSWORD"),
     )
-    for l in blacklist_listings:
+    cursor = connection.cursor()
+    # Get each listing with their summary and a pipe separated list of associated job statuses
+    cursor.execute(
+        """
+        SELECT l.id, summary, STRING_AGG(status, '|')
+        FROM jobtolisting
+                 JOIN public.job j ON j.id = jobtolisting.job_id
+                 JOIN public.listing l ON l.id = jobtolisting.listing_id
+        GROUP BY l.id
+        """
+    )
+    need_blacklist = []
+    need_summary = []
+    for listing in cursor.fetchall():
+        has_blacklist = [x == "blacklist" for x in listing[2].split("|")]
+        if all(has_blacklist):
+            if listing[1] != "blacklist":
+                # Listings with only blacklist status jobs and without the "blacklist" summary
+                need_blacklist.append(listing[0])
+        elif any(has_blacklist) and listing[1] == "blacklist":
+            # Listings that have previously been given the "blacklist" summary, but now have an associated job that isn't blacklisted
+            need_summary.append(listing[0])
+        elif listing[1] in ["", "N/A"] and any(x in ["new", "interested"] for x in listing[2].split("|")):
+            # Remaining jobs without summaries that are still relevant to users
+            need_summary.append(listing[0])
+    connection.close()
+
+    need_blacklist = Listing.select().where(Listing.id << need_blacklist)
+    for l in need_blacklist:
         l.summary = "blacklist"
         l.save()
-    listings_to_summaries = (
-        Listing.select()
-        .join(JobToListing)
-        .join(Job)
-        .where(Job.status << ["new", "interested"], Listing.summary << ["", "N/A"])
-        .execute()
-    )
-    for listing in tqdm(listings_to_summaries):
+
+    need_summary = Listing.select().where(Listing.id << need_summary)
+    for listing in tqdm(need_summary):
         try:
             with open(util.description_path(listing)) as f:
                 summarise_and_save(f.read(), listing)
         except FileNotFoundError as e:
+            # This will fail if the listing has been archived, but jobs old enough to be archived are no longer relevant
             print(f"File for listing {listing.id} not found: {e}")
             summarise_and_save("", listing)
 
